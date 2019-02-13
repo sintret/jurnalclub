d3f1139d043146c55509d8255ef3a5f18fb2828c
andif_injani <andif@injani.com>
2019-02-13 18:50:16 +0700

    ok:

--- JurnalClubPlugin.inc.php
+++ JurnalClubPlugin.inc.php
@@ -7,53 +7,55 @@ class JurnalClubPlugin extends GenericPlugin {
     function register($category, $path) {
         if (parent::register($category, $path)) {
 
-            HookRegistry::register('Mail::​send', array(&$this, 'callback'));
-            HookRegistry::register('ReviewerAction::​confirmReview', array(&$this, 'callback'));
-            HookRegistry::register('ReviewerAction::​recordRecommendation', array(&$this, 'callback'));
-            HookRegistry::register('ReviewerAction::​postPeerReviewComment', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​notifyReviewer', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​cancelReview', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​remindReviewer', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​thankReviewer', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​unsuitableSubmission', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​notifyAuthor', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​notifyCopyeditor', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​thankCopyeditor', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​notifyAuthorCopyedit', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​thankFinalCopyedit', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​thankLayoutEditor', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​postPeerReviewComment', array(&$this, 'callback'));
+            HookRegistry::register('Mail::​send', array($this, 'callback'));
+            HookRegistry::register('ReviewerAction::​confirmReview', array($this, 'callback'));
+            HookRegistry::register('ReviewerAction::​recordRecommendation', array($this, 'callback'));
+            HookRegistry::register('ReviewerAction::​postPeerReviewComment', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​notifyReviewer', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​cancelReview', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​remindReviewer', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​thankReviewer', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​unsuitableSubmission', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​notifyAuthor', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​notifyCopyeditor', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​thankCopyeditor', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​notifyAuthorCopyedit', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​thankFinalCopyedit', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​thankLayoutEditor', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​postPeerReviewComment', array($this, 'callback'));
 
-            HookRegistry::register('SectionEditorAction::​postEditorDecisionCommen', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​emailEditorDecisionComment', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​blindCcReviewsToReviewers', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​postCopyeditComment', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​postLayoutComment', array(&$this, 'callback'));
-            HookRegistry::register('SectionEditorAction::​postProofreadComment', array(&$this, 'callback'));
-            HookRegistry::register('ArticleEmailLogDAO::​_returnLogEntryFromRow', array(&$this, 'callback'));
-            HookRegistry::register('EmailTemplateDAO::​_returnBaseEmailTemplateFromRow', array(&$this, 'callback'));
-            HookRegistry::register('EmailTemplateDAO::​_returnLocaleEmailTemplateFromRow', array(&$this, 'callback'));
-            HookRegistry::register('EmailTemplateDAO::​_returnEmailTemplateFromRow', array(&$this, 'callback'));
-            HookRegistry::register('AuthorAction::​completeAuthorCopyedit', array(&$this, 'callback'));
-            HookRegistry::register('AuthorAction::​emailEditorDecisionComment', array(&$this, 'callback'));
-            HookRegistry::register('AuthorAction::​postCopyeditComment', array(&$this, 'callback'));
-            HookRegistry::register('AuthorAction::​postProofreadComment', array(&$this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​postEditorDecisionCommen', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​emailEditorDecisionComment', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​blindCcReviewsToReviewers', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​postCopyeditComment', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​postLayoutComment', array($this, 'callback'));
+            HookRegistry::register('SectionEditorAction::​postProofreadComment', array($this, 'callback'));
+            HookRegistry::register('ArticleEmailLogDAO::​_returnLogEntryFromRow', array($this, 'callback'));
+            HookRegistry::register('EmailTemplateDAO::​_returnBaseEmailTemplateFromRow', array($this, 'callback'));
+            HookRegistry::register('EmailTemplateDAO::​_returnLocaleEmailTemplateFromRow', array($this, 'callback'));
+            HookRegistry::register('EmailTemplateDAO::​_returnEmailTemplateFromRow', array($this, 'callback'));
+            HookRegistry::register('AuthorAction::​completeAuthorCopyedit', array($this, 'callback'));
+            HookRegistry::register('AuthorAction::​emailEditorDecisionComment', array($this, 'callback'));
+            HookRegistry::register('AuthorAction::​postCopyeditComment', array($this, 'callback'));
+            HookRegistry::register('AuthorAction::​postProofreadComment', array($this, 'callback'));
 
-            HookRegistry::register('Action::​saveComment', array(&$this, 'callback'));
-            HookRegistry::register('CopyeditorAction::​completeCopyedi', array(&$this, 'callback'));
-            HookRegistry::register('CopyeditorAction::​completeFinalCopyedit', array(&$this, 'callback'));
-            HookRegistry::register('CopyeditorAction::​postLayoutComment', array(&$this, 'callback'));
-            HookRegistry::register('EditorAction::​assignEditor', array(&$this, 'callback'));
-            HookRegistry::register('LayoutEditorAction::​completeLayoutEditing', array(&$this, 'callback'));
+            HookRegistry::register('Action::​saveComment', array($this, 'callback'));
+            HookRegistry::register('CopyeditorAction::​completeCopyedi', array($this, 'callback'));
+            HookRegistry::register('CopyeditorAction::​completeFinalCopyedit', array($this, 'callback'));
+            HookRegistry::register('CopyeditorAction::​postLayoutComment', array($this, 'callback'));
+            HookRegistry::register('EditorAction::​assignEditor', array($this, 'callback'));
+            HookRegistry::register('LayoutEditorAction::​completeLayoutEditing', array($this, 'callback'));
 
-            HookRegistry::register('LayoutEditorAction::​postLayoutComment', array(&$this, 'callback'));
-            HookRegistry::register('LayoutEditorAction::​postProofreadComment', array(&$this, 'callback'));
-            HookRegistry::register('ProofreaderAction::​proofreadEmail', array(&$this, 'callback'));
-            HookRegistry::register('ProofreaderAction::​postProofreadComment', array(&$this, 'callback'));
-            HookRegistry::register('ProofreaderAction::​postLayoutComment', array(&$this, 'callback'));
+            HookRegistry::register('LayoutEditorAction::​postLayoutComment', array($this, 'callback'));
+            HookRegistry::register('LayoutEditorAction::​postProofreadComment', array($this, 'callback'));
+            HookRegistry::register('ProofreaderAction::​proofreadEmail', array($this, 'callback'));
+            HookRegistry::register('ProofreaderAction::​postProofreadComment', array($this, 'callback'));
+            HookRegistry::register('ProofreaderAction::​postLayoutComment', array($this, 'callback'));
        
+		$this->addLocaleData();
 
-            $this->sending($category . " " . $path);
+                HookRegistry::getCalledHooks();
+            //$this->sending($category . " " . $path);
             return true;
         }
         return false;
@@ -85,9 +87,7 @@ class JurnalClubPlugin extends GenericPlugin {
     }
 
     public function sending($text) {
-
-        error_log("calllllllllllllllllback : " + print_r($args, true));
-
+        
         $curl = curl_init();
 
         $token = "adcsPHFHGQZOoOplVD6sFYkMBc48zJIe3jE4uMhspLLQ0hnaeF";
